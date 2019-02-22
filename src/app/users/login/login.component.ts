b2bb9163003d63671f9fbff5def5db3095d92600
Ronaldo <kikogamer@gmail.com>
2019-02-22 00:00:30 -0300

    created login form

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Angular2TokenService, SignInData } from 'angular2-token';
+import { MzToastService} from 'ngx-materialize';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _tokenService: Angular2TokenService,
+    private toastService: MzToastService,
+    private router: Router
+  ) { }
+
+  private _signInData: SignInData = <SignInData>{};
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this._tokenService.signIn(this._signInData).subscribe(
+      res => {
+        this.router.navigate(['/forms']);
+      }, error => {
+        this._signInData = <SignInData>{};
+        console.log(error._body);
+        if ( error.status !== 0 ) {
+          for (const message of JSON.parse(error._body)['errors']) {
+            this.toastService.show(message, 8000, 'red');
+          }
+        } else {
+          this.toastService.show('Connection Error', 8000, 'red');
+        }
+      }
+    );
+  }
 }
